5f04865729632a711c0f178e2f0f00fec81aec81
GenesisFan64 <genesisfan64@gmail.com>
2020-10-06 04:06:33 -0500

    It works now.

--- system/macros.asm
+++ system/macros.asm
@@ -1,6 +1,6 @@
 ; ===========================================================================
 ; ----------------------------------------------------------------
-; MACROS
+; Memory map
 ; ----------------------------------------------------------------
 
 ; --------------------------------------------------------
@@ -14,3 +14,71 @@
 		supmode on 			; Supervisor mode
 		dottedstructs off		; If needed
 		page 0
+
+; -------------------------------------
+; ZERO Fill padding
+; 
+; if AS align doesn't work
+; -------------------------------------
+
+rompad		macro address			; Zero fill
+diff := address - *
+		if diff < 0
+			error "too much stuff before org $\{address} ($\{(-diff)} bytes)"
+		else
+			while diff > 1024
+				; AS can only generate 1 kb of code on a single line
+				dc.b [1024]0
+diff := diff - 1024
+			endm
+			dc.b [diff]0
+		endif
+	endm
+
+; ====================================================================
+; ---------------------------------------------
+; ISO filesystem macros
+; ---------------------------------------------
+
+; Set a ISO file
+; NOTE: a valid ISO head is required ($8000 to $B7FF)
+
+iso_setfs	macro type,start,end
+.fstrt:		dc.b .fend-.fstrt				; Block size
+		dc.b 0						; zero
+		dc.b (start>>11&$FF),(start>>19&$FF)		; Start sector, little
+		dc.b (start>>27&$FF),(start>>35&$FF)
+		dc.l start>>11					; Start sector, big
+		dc.b ((end-start)&$FF),((end-start)>>8&$FF)	; Filesize, little
+		dc.b ((end-start)>>16&$FF),((end-start)>>24&$FF)
+		dc.l end-start					; Filesize, big
+		dc.b (2018-1900)+1				; Year
+		dc.b 0,0,0,0,0,0				; TODO
+		dc.b 2						; File flags
+		dc.b 0,0
+		dc.b 1,0					; Volume sequence number, little
+		dc.b 0,1					; Volume sequence number, big
+		dc.b 1,type
+.fend:
+		endm
+		
+iso_file	macro filename,start,end
+.fstrt:		dc.b .fend-.fstrt				; Block size
+		dc.b 0						; zero
+		dc.b (start>>11&$FF),(start>>19&$FF)		; Start sector, little
+		dc.b (start>>27&$FF),(start>>35&$FF)
+		dc.l start>>11					; Start sector, big
+		dc.b ((end-start)&$FF),((end-start)>>8&$FF)	; Filesize, little
+		dc.b ((end-start)>>16&$FF),((end-start)>>24&$FF)
+		dc.l end-start					; Filesize, big
+		dc.b (2018-1900)+1				; Year
+		dc.b 0,0,0,0,0,0				; TODO
+		dc.b 0						; File flags
+		dc.b 0,0
+		dc.b 1,0					; Volume sequence number, little
+		dc.b 0,1					; Volume sequence number, big
+		dc.b .flend-.flen
+.flen:		dc.b filename,";1"
+.flend:		dc.b 0
+.fend:
+		endm
---
+++ system/map.asm
@@ -0,0 +1,38 @@
+; ===========================================================================
+; ----------------------------------------------------------------
+; Memory map
+; ----------------------------------------------------------------
+
+; 0x00000000 	0x000FFFFF 	Boot ROM 	Repets every 512 kb
+; 0x00100000 	0x0017FFFF 	SMPC registers 	 
+; 0x00180000 	0x001FFFFF 	Backup RAM 	 
+; 0x00200000 	0x002FFFFF 	Work RAM Low 	 
+; 0x00300000 	0x003FFFFF 	Random data 	Random on every read (mostly 0x00)
+; 0x00400000 	0x007FFFFF 	Zero 	Always returns 0x0000
+; 0x00800000 	0x00FFFFFF 	Unknown 	Always returns 0x00000001000200030004000500060007
+; 0x01000000 	0x017FFFFF 	Master Init (MINIT) 	Data written goes to Master SH-2 Free Running Timer. Section is Write Only.
+; 0x01800000 	0x01FFFFFF 	Slave Init (SINIT) 	Data written goes to Slave SH-2 Free Running Timer. Section is Write Only.
+; 0x02000000 	0x03FFFFFF 	CS0 	Cartridge 0
+; 0x04000000 	0x04FFFFFF 	CS1 	Cartridge 1
+; 0x05000000 	0x057FFFFF 	Dummy 	 
+; 0x05800000 	0x058FFFFF 	CD-ROM registers (CS2) 	Repeats every 64 Bytes
+; 0x05900000 	0x059FFFFF 	Unknown 	It will lockup the saturn when read
+; 0x05A00000 	0x05AFFFFF 	68000 Audio Work RAM 	Stores audio data that will be played
+; 0x05B00000 	0x05BFFFFF 	SCSP registers 	 
+; 0x05C00000 	0x05C7FFFF 	VDP1 VRAM 	Video RAM for the 3D VDP1
+; 0x05C80000 	0x05CFFFFF 	VDP1 Framebuffer 	Current Image Buffer for the VDP1 to render to
+; 0x05D00000 	0x05D7FFFF 	VDP1 Registers 	VDP1 variables that change the functionality of how the VDP1 operates
+; 0x05D80000 	0x05DFFFFF 	Lockup when read 	 
+; 0x05E00000 	0x05EFFFFF 	VDP2 VRAM 	Video Display Processor 2 (Background) Video RAM - Repeats every 512 kb
+; 0x05F00000 	0x05F7FFFF 	VDP2 CRAM 	VDP2 Color RAM - Basically the background Palette - Repeats every 4kb
+; 0x05F80000 	0x05FBFFFF 	VDP2 registers 	VDP2 variables that change the functionality of how the VDP2 operates - Repeats every 512 bytes
+; 0x05FC0000 	0x05FDFFFF 	Unknown 	Always returns 0x000E0000
+; 0x05FE0000 	0x05FEFFFF 	SCU registers 	Repeats every 256 bytes
+; 0x05FF0000 	0x05FFFFFF 	Unknown registers 	Repeats every 256 bytes
+; 0x06000000 	0x07FFFFFF 	Work RAM High 	 
+
+
+VDP2_VRAM	equ	0x25E00000
+VDP2_CRAM	equ	0x25F00000
+VDP2_REG	equ	0x25F80000
+
